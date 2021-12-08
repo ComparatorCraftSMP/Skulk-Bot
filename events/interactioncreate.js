d71d3aa49f50d8a82ed86b11a33d202117252ac7
BossDaily <comparatorcraft2020@gmail.com>
2021-12-08 16:09:28 +0000

    Added interaction create event

---
+++ events/interactioncreate.js
@@ -0,0 +1,6 @@
+module.exports = {
+	name: 'interactionCreate',
+	execute(interaction) {
+		console.log(`At ${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.type}.`);
+	},
+};
