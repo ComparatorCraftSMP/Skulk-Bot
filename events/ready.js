25650e9e4a1da4c0492fce49a136478074762ebe
BossDaily <comparatorcraft2020@gmail.com>
2021-12-22 20:39:52 -0600

    Chaned ready message

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag} at ${client.readyAt}`);
+		console.log(`The bot is up! Logged in as ${client.user.tag} at ${client.readyAt}`);
 	},
 };
