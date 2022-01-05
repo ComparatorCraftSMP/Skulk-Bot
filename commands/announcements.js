fc14c87a609eecd080d024487e4446871785b8ba
BossDaily <comparatorcraft2020@gmail.com>
2022-01-05 08:21:25 -0600

    Added the slash command builder

--- commands/announcements.js
+++ commands/announcements.js
@@ -1,2 +1,11 @@
 const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
 const {MessageEmbed, CommandInteractionOptionResolver} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('announce')
+        .setDescription('this sends an embed in the channel you choose')
+        .addChannelOption(option => option.setName('channel').setDescription('this is the channel that the announcement will be sent in'))
+
+
+}
