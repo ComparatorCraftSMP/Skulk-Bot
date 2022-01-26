343ea15dca1295574319505a99a7341b477bf528
BossDaily <comparatorcraft2020@gmail.com>
2022-01-25 19:35:57 -0600

    Added boiler plate code for the anti virus module

---
+++ events/checkVirus.js
@@ -0,0 +1,9 @@
+const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
+const {MessageEmbed, CommandInteractionOptionResolver, Message} = require('discord.js');
+
+module.export = {
+    name: '',
+    execute() {
+        
+    }
+}
