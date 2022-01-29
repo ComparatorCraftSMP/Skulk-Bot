090f849d0db55991df97b36ce9d238aa175b0aed
BossDaily <comparatorcraft2020@gmail.com>
2022-01-29 14:29:24 -0600

    now logs in console the username that sent the url

--- events/checkVirus.js
+++ events/checkVirus.js
@@ -3,8 +3,8 @@ module.exports = {
     execute(msg) {
         const urlCheck = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?");
         if(urlCheck.test(msg)) {
-            console.log("It has an URL!");
-            console.log(urlCheck.exec(msg)[0]);
+            console.log(`${msg.author.tag} sent a URL \nURL: ${urlCheck.exec(msg)[0]}`);
+            
         }
         if(msg.attachments){
             console.log(`Attached ${msg.attachments.size} message sent at ${msg.createdAt}`)
