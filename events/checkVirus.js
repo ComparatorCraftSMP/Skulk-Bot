f2d92b9c721b62de2694347f772c6edde8ef47bc
BossDaily <comparatorcraft2020@gmail.com>
2022-01-29 14:23:59 -0600

    added a check for if a message has a url or not (and also can get the url)

--- events/checkVirus.js
+++ events/checkVirus.js
@@ -1,10 +1,13 @@
 module.exports = {
     name: 'messageCreate',
     execute(msg) {
-        
-        if(msg.attachments){
-            console.log(`Attached message sent at ${msg.createdAt}`)
+        const urlCheck = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?");
+        if(urlCheck.test(msg)) {
+            console.log("It has an URL!");
+            console.log(urlCheck.exec(msg)[0]);
         }
-        
+        if(msg.attachments){
+            console.log(`Attached ${msg.attachments.size} message sent at ${msg.createdAt}`)
+        }
     }
 }
