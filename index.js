2f60b67e27d184f82bb7fb77c8aeef58d93cca35
BossDaily <comparatorcraft2020@gmail.com>
2022-01-29 10:35:42 -0600

    Added more intents

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const { clientId, guildId} = require('./config.json');
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const { Sequelize, DataTypes, Model} = require('sequelize');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const Shops = require('./utils/dbmodel.js');
+const client = new Client({ intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_MESSAGE_TYPING,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+            Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+        ],
+        partials: [
+            'CHANNEL', // Required to receive DMs
+        ] });
 
 client.commands = new Collection();
 
@@ -47,14 +56,6 @@ client.on('interactionCreate', async interaction => {
     }
 })
 
-//Sequelize connection info
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
-});
 
 
 //This is what logs the bot in
