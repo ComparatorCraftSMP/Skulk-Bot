435ae8d7234ae82e922042dc0db8f70b3e3bb287
BossDaily <comparatorcraft2020@gmail.com>
2022-01-30 09:42:25 -0600

    added proper import and checkvirus is now being used

--- events/antivirus.js
+++ events/antivirus.js
@@ -1,3 +1,7 @@
+const { checkVirus } = require("../utils/checkvirus");
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
     name: 'messageCreate',
     execute(msg) {
@@ -6,6 +10,7 @@ module.exports = {
         const urlCheck = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?");
         if(urlCheck.test(msg)) {
             console.log(`${msg.author.tag} sent a URL \nURL: ${urlCheck.exec(msg)[0]}`);
+            checkVirus(`${urlCheck.exec(msg)[0]}`)
         }
         //checks if message sent has an attachment
         if(msg.attachments){
